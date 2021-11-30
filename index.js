2f52b507798f0b7d30c6024181a0f15ba70f2a75
Kushang Gajjar <kgajjar@salesforce.com>
2021-11-29 20:10:31 -0500

    set server url

--- index.js
+++ index.js
@@ -1,63 +1,69 @@
-const { Eyes, Target } = require('@wdio/eyes.webdriverio');
-let eyes = new Eyes();
+const { Eyes, Target } = require("@wdio/eyes.webdriverio");
+let eyes;
 let Helper = codecept_helper;
 let windowsSize;
 let appName;
 let client;
 
 class ApplitoolsHelper extends Helper {
+  constructor(config) {
+    super(config);
+    this.config = config;
+    appName = config.appName || "Application Under Test";
+    eyes = new Eyes(config.serverUrl || "");
+  }
 
-    constructor(config) {
-        super(config);
-        this.config = config;
-        appName = config.appName || 'Application Under Test';
+  async _beforeSuite() {
+    this.helpers["WebDriver"].config.manualStart = true;
+    this.helpers["WebDriver"].options.manualStart = true;
+    if (this.config.windowSize) {
+      windowsSize = this._getWindowsSize(this.config);
+    } else if (this.helpers["WebDriver"].config.windowSize) {
+      windowsSize = this._getWindowsSize(this.helpers["WebDriver"].config);
+    } else {
+      windowsSize = { width: 1920, height: 600 };
+    }
+    if (client) {
+      await this.helpers["WebDriver"]._stopBrowser();
+    }
+    client = await this.helpers["WebDriver"]._startBrowser();
+  }
+
+  _getWindowsSize(config) {
+    return {
+      width: parseInt(config.windowSize.split("x")[0], 10),
+      height: parseInt(config.windowSize.split("x")[1], 10),
+    };
+  }
+
+  _generateRandomString() {
+    return (
+      Math.random().toString(36).substring(2, 15) +
+      Math.random().toString(36).substring(2, 15)
+    );
+  }
+
+  /**
+   * @param pageName {String} name of the page you want to check
+   * @param uniqueId {String} provide a unique id to combine tests into a batch
+   * @param matchLevel {String} set the match level. Possible values: Extract, Strict, Content, Layout
+   *
+   */
+  async eyeCheck(pageName, uniqueId, matchLevel) {
+    eyes.setApiKey(this.config.applitoolsKey);
+    eyes.setForceFullPageScreenshot(true);
+    if (uniqueId) {
+      eyes.setBatch(pageName, uniqueId);
     }
 
-    async _beforeSuite() {
-        this.helpers['WebDriver'].config.manualStart = true;
-        this.helpers['WebDriver'].options.manualStart = true;
-        if (this.config.windowSize) {
-            windowsSize = this._getWindowsSize(this.config);
-        } else if (this.helpers['WebDriver'].config.windowSize) {
-            windowsSize = this._getWindowsSize(this.helpers['WebDriver'].config);
-        } else {
-            windowsSize = {'width': 1920, 'height': 600};
-        }
-        if (client) {
-            await this.helpers['WebDriver']._stopBrowser();
-        }
-        client = await this.helpers['WebDriver']._startBrowser();
+    if (matchLevel) {
+      eyes.setMatchLevel(matchLevel);
     }
 
-    _getWindowsSize(config) {
-        return { width: parseInt(config.windowSize.split('x')[0], 10), height: parseInt(config.windowSize.split('x')[1], 10) }
-    }
-
-    _generateRandomString() {
-        return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-    }
-
-    /**
-    * @param pageName {String} name of the page you want to check
-    * @param uniqueId {String} provide a unique id to combine tests into a batch
-    * @param matchLevel {String} set the match level. Possible values: Extract, Strict, Content, Layout
-    * 
-     */
-    async eyeCheck(pageName, uniqueId, matchLevel) {
-        eyes.setApiKey(this.config.applitoolsKey);
-        eyes.setForceFullPageScreenshot(true);
-        if (uniqueId) {
-            eyes.setBatch(pageName, uniqueId); 
-        }
-
-        if (matchLevel) {
-            eyes.setMatchLevel(matchLevel);
-        }
-        
-        await eyes.open(client, appName, pageName, windowsSize);
-        await eyes.check(pageName, Target.window());
-        await eyes.close();
-    }
+    await eyes.open(client, appName, pageName, windowsSize);
+    await eyes.check(pageName, Target.window());
+    await eyes.close();
+  }
 }
 
 module.exports = ApplitoolsHelper;
